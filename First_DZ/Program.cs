a3af21f20e505801c49191755c1c25f327140fc6
yanepumba <adress>
2023-10-22 12:58:33 +0300

    add Second_DZ

--- First_DZ/Program.cs
+++ First_DZ/Program.cs
@@ -2,15 +2,15 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 Console.WriteLine("Введите число A:");
-        int A = Convert.ToInt32(Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите число B:");
-        int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
 
-        int result = 1;
-        for (int i = 0; i < B; i++)
-        {
-            result *= A;
-        }
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
 
-        Console.WriteLine("Результат: " + result);
+Console.WriteLine("Результат: " + result);
---
+++ Second_DZ/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = GetDigitSum(number);
+Console.WriteLine($"Сумма цифр числа {number}: {sum}");
+
+
+int GetDigitSum(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+
+    return sum;
+}
+
+
+
+
